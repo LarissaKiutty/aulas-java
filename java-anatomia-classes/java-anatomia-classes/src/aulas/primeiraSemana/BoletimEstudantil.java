ea1e1f8586f54e0d09ae941a088956ab604576dd
LarissaKiutty <larissa.souza1941@gmail.com>
2024-06-10 17:36:36 -0300

    REFACTOR: organizing files and creating new packages

--- java-anatomia-classes/java-anatomia-classes/src/aulas/primeiraSemana/BoletimEstudantil.java
+++ java-anatomia-classes/java-anatomia-classes/src/aulas/primeiraSemana/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package aulas.primeiraSemana;
 public class BoletimEstudantil {
   public static void main(String[] args) {
     int mediaFinal = 4;
--- java-anatomia-classes/java-anatomia-classes/src/aulas/primeiraSemana/MinhaClasse.java
+++ java-anatomia-classes/java-anatomia-classes/src/aulas/primeiraSemana/MinhaClasse.java
@@ -1,3 +1,4 @@
+package aulas.primeiraSemana;
 public class MinhaClasse {
   public static void main(String [] args) {
 
