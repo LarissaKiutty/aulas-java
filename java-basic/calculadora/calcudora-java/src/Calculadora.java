67a91ac94617a6e76e9cac89e0b57ddb73b3a6cd
LarissaKiutty <larissa.souza1941@gmail.com>
2024-06-26 17:49:54 -0300

    FEAT: creating a lesson package with the Calculator class, where I learned about documentation in Java.

---
+++ java-basic/calculadora/calcudora-java/src/Calculadora.java
@@ -0,0 +1,27 @@
+// TRINANDO DOCUMENTAÇÃO
+
+/**
+ * <h1> Calculadora </h1>
+ * 
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * 
+ * <b> Note: </b> Leia atentamente a documentação desta classes para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Larissa Kiutty
+ * @version 1.0
+ * @since 26/06/24
+ */
+
+ public class Calculadora {
+  /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   * 
+   */
+  
+   public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+   }
+ }
